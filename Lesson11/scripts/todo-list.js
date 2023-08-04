de4d4d4d194c540bdd53daba6bab62471db43781
unknown <tejasbhanushali.1@gmail.com>
2023-08-04 13:31:28 +0530

    completed 2nd project of array

--- Lesson11/scripts/todo-list.js
+++ Lesson11/scripts/todo-list.js
@@ -1,7 +1,24 @@
 const todoArray = [];
+
+renderTodoList();
+
+function renderTodoList(){
+  let todoListHTML = '';
+
+  for(let i = 0; i < todoArray.length; i++){
+    const todo = todoArray[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+    document.querySelector('.js-todo-list')
+      .innerHTML = todoListHTML;
+  }
+  console.log(todoListHTML);
+}
+
     function pushTodo(){
       const inputElement = document.querySelector('.input-todo')
       todoArray.push(inputElement.value);
       console.log(todoArray);
       inputElement.value = '';
+      renderTodoList();
     }
